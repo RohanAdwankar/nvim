cb4e4a8412f885cebc10657621d97c628524b5e2
Rohan Adwankar <39285979+RohanAdwankar@users.noreply.github.com>
2025-04-27 14:42:15 -0700

    init

---
+++ init.lua
@@ -0,0 +1,29 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.mouse = "a"
+vim.opt.clipboard = "unnamedplus"
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.smartindent = true
+vim.opt.wrap = false
+vim.api.nvim_set_keymap('i', 'ii', '<Esc>', { noremap = true })
+vim.opt.rtp:prepend("~/.config/nvim/lazy/lazy.nvim")
+
+require("lazy").setup({
+  { "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
+  { "nvim-tree/nvim-tree.lua", dependencies = { "nvim-tree/nvim-web-devicons" } },
+  { "nvim-lualine/lualine.nvim", dependencies = { "nvim-tree/nvim-web-devicons" } },
+  { "lewis6991/gitsigns.nvim" },
+})
+
+vim.keymap.set("n", "<leader>ff", require("telescope.builtin").find_files, {})
+vim.keymap.set("n", "<leader>fg", require("telescope.builtin").live_grep, {})
+vim.keymap.set("n", "<leader>fb", require("telescope.builtin").buffers, {})
+vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", {})
+
+require('lualine').setup()
+require('nvim-tree').setup()
+require('gitsigns').setup()
