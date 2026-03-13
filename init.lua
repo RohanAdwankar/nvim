f2ae54aeef40e90558201a7dec57d22530526b56
Rohan Adwankar <39285979+RohanAdwankar@users.noreply.github.com>
2026-03-12 22:56:17 -0700

    better ergo on panel openings, markdown rendering, and some tree shouldnt ignore things

--- init.lua
+++ init.lua
@@ -43,6 +43,18 @@ map("n", "<leader>tt", ":belowright split | terminal<CR>", { silent = true })
 -- Buffer navigation
 map("n", "<Space>n", "<C-i>")
 map("n", "<Space>m", "<C-o>")
+-- Open Panes
+local function my_on_attach(bufnr)
+  local api = require('nvim-tree.api')
+
+  local function opts(desc)
+    return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+  api.config.mappings.default_on_attach(bufnr)
+  vim.keymap.set('n', 'v', api.node.open.vertical, opts('Open: Vertical Split'))
+  vim.keymap.set('n', 's', api.node.open.horizontal, opts('Open: Horizontal Split'))
+  vim.keymap.set('n', 't', api.node.open.tab, opts('Open: New Tab'))
+end
 
 -- Scratch buffer for messing around
 vim.api.nvim_create_user_command("Scratch", function()
@@ -100,6 +112,16 @@ require("lazy").setup({
   --    "nvim-lua/plenary.nvim"
   --  }
   -- },
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' },            -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' },        -- if you use standalone mini plugins
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+  }
+  
 })
 
 -- Basic Colorscheme
@@ -112,7 +134,18 @@ vim.cmd("colorscheme retrobox")
 
 -- Plugin Setups
 require("lualine").setup()
-require("nvim-tree").setup({update_focused_file = { enable = true }})
+require("nvim-tree").setup({
+  git = {
+    ignore = false,
+  },
+  filters = {
+    dotfiles = false,
+  },
+  update_focused_file = { 
+    enable = true
+  },
+  on_attach= my_on_attach,
+})
 require("gitsigns").setup()
 -- require("CopilotChat").setup({
 --   context = {"files"},
